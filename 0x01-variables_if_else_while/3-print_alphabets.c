fa56510a107ad2816213ed11804987c09ec26d30
Sibusisiwe <sisim224@gmail.com>
2023-09-15 19:37:27 -0700

    3-print_alphabets.c project

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out all the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alper[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
